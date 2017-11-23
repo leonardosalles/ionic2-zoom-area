024ac77780329b767f5cb350683051fdbeaab271
Leonardo Salles <leosallestm@gmail.com>
2017-11-23 14:47:39 -0200

    Fix #6 - LazyLoading Modules

--- src/zoom-area.module.ts
+++ src/zoom-area.module.ts
@@ -2,15 +2,13 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { IonicModule } from 'ionic-angular';
 import { ZoomAreaComponent } from './zoom-area.component';
 import { ZoomAreaProvider } from './zoom-area.provider';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     ZoomAreaComponent
   ],
   imports: [
-    IonicModule,
-    BrowserAnimationsModule
+    IonicModule
   ],
   exports: [
     ZoomAreaComponent
